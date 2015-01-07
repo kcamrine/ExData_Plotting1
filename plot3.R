359a0b7e42c4d071776cdbe3afb0120cc0777d65
kcamrine <kc.amrine@gmail.com>
2015-01-07 10:55:38 -0800

    add plot 3

---
+++ plot3.R
@@ -0,0 +1,27 @@
+#### packages needed ###
+library('lubridate')
+
+#### load and subset data ###
+household_power_consumption <- read.table("household_power_consumption.txt",
+                                          sep=";",header=TRUE,
+                                          colClasses=c("character",
+                                                       "character",rep("numeric",7)),
+                                          na.strings=c("?")) #load data
+
+Date.Time <- strptime(paste(household_power_consumption$Date,
+                            household_power_consumption$Time),
+                      "%d/%m/%Y %H:%M:%S") #determine dates
+
+get.dates <- interval(strptime("2007-02-01 00:00:01","%Y-%m-%d %H:%M:%S"),
+                      strptime("2007-02-02 23:59:00","%Y-%m-%d %H:%M:%S")) #define interval
+household_power_consumption <- household_power_consumption[na.exclude(Date.Time
+                                                                      %within% get.dates),] #keep data in interval
+
+#### plot data ###
+png("plot3.png",480,480,bg="transparent")
+plot(household_power_consumption$Sub_metering_1,type="l",ylab="Energy sub metering",xaxt = "n",xlab = "")
+points(household_power_consumption$Sub_metering_2,type="l",col="red")
+points(household_power_consumption$Sub_metering_3,type="l",col="blue")
+axis(side=1,at=c(1,which(household_power_consumption$Date %in% names(table(household_power_consumption[,1]))[2])[1],length(household_power_consumption[,1])),labels=c("Thu","Fri","Sat"))
+legend("topright",colnames(household_power_consumption)[7:9],col=c("black","red","blue"),lty=1)
+dev.off()
